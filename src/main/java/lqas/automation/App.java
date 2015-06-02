9429f06019d5d5aab2b9bb17069877b800f43ddb
tlytvyn <tlytvyn@lohika.com>
2015-06-02 20:50:09 +0300

    my first commit to repository

---
+++ src/main/java/lqas/automation/App.java
@@ -0,0 +1,13 @@
+package lqas.automation;
+
+/**
+ * Hello world!
+ * mvn exec:java -Dexec.mainClass="com.example.Main"
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
